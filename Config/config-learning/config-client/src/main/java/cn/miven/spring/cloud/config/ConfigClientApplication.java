854ce1109dae805a9de3ef25cafe5ac2fb06c76b
mingzhi.xie <mingzhi.xie@yintech.cn>
2019-02-11 14:46:07 +0800

    update Spring Cloud Bus

--- Config/config-learning/config-client/src/main/java/cn/miven/spring/cloud/config/ConfigClientApplication.java
+++ Config/config-learning/config-client/src/main/java/cn/miven/spring/cloud/config/ConfigClientApplication.java
@@ -3,7 +3,6 @@ package cn.miven.spring.cloud.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
