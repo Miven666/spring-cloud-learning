e218ed9be9308bdce383b4053d2ffd5fd7adbc29
mingzhi.xie <xie6032mail@163.com>
2019-09-09 09:57:24 +0800

    优化包结构

---
+++ feign-service/src/main/java/cn/miven/spring/cloud/feign/FeignServiceApplication.java
@@ -0,0 +1,20 @@
+package cn.miven.spring.cloud.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author mingzhi.xie
+ * @date 2019/1/22.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class FeignServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignServiceApplication.class, args);
+    }
+}
---
+++ sleuth-one/src/main/java/cn/miven/spring/cloud/sleuth/controller/HomeController.java
@@ -0,0 +1,33 @@
+package cn.miven.spring.cloud.sleuth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author mingzhi.xie
+ * @date 2019/2/11.
+ */
+@RestController
+public class HomeController {
+
+    private static final Logger logger = Logger.getLogger(HomeController.class.getName());
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/one")
+    public String callHome(){
+        logger.log(Level.INFO, "calling trace server-one  ");
+        return restTemplate.getForObject("http://localhost:8662/two", String.class);
+    }
+    @RequestMapping("/info")
+    public String info(){
+        logger.log(Level.INFO, "calling trace server-one  ");
+        return "I'm server-one";
+    }
+}
